650e44bf6d1a3410d998d1783848d02c82dee3b3
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-23 23:06:46 -0500

    fix: add function Factory.predict(address,address)

--- src/Factory.sol
+++ src/Factory.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.28;
 
 import {Channel} from "./Channel.sol";
 
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract Factory is EIP712("Factory", "1") {
@@ -10,5 +11,10 @@ contract Factory is EIP712("Factory", "1") {
 
     constructor() {
         implementation = address(new Channel());
+    }
+
+    function predict(address alice, address bob) external view returns (address) {
+        return
+            Clones.predictDeterministicAddress(implementation, bytes32(uint256(uint160(alice)) ^ uint256(uint160(bob))));
     }
 }
