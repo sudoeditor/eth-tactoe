9e97b8d54d0e2525c5bc850a4982e2502ac00d88
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-24 13:31:57 -0500

    fix: replace Initializable with EIP712Upgradeable in Channel.sol

--- src/Channel.sol
+++ src/Channel.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.28;
 
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {EIP712Upgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
 
-contract Channel is Initializable {
+contract Channel is EIP712Upgradeable {
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() external initializer {}
+    function initialize() external initializer {
+        __EIP712_init("Channel", "1");
+    }
 }
