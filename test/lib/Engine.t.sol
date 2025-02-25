292399f6aedb107109af23437ca60e0499ae8d90
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 13:38:16 -0500

    fix(test): add lib/Engine.t.sol

---
+++ test/lib/Engine.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.28;
+
+import {Engine} from "../../src/lib/Engine.sol";
+
+import {Test} from "forge-std/Test.sol";
+
+contract EngineTest is Test {}
