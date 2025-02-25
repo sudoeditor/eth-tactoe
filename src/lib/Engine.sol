4bf141f814c1589419197513b0149d7a3ddae3af
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 00:24:16 -0500

    fix: add Grid type and function Engine.toGrid(uint16)

--- src/lib/Engine.sol
+++ src/lib/Engine.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.28;
 
-library Engine {}
+library Engine {
+    type Grid is uint16;
+
+    function toGrid(uint16 value) internal pure returns (Grid) {
+        return Grid.wrap(value & 0x1ff);
+    }
+}
