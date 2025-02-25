befc55a1c5fdd0060b11e7cc3219f4b14b6be435
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 00:26:03 -0500

    fix: add function Engine.equal(Grid,Grid)

--- src/lib/Engine.sol
+++ src/lib/Engine.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.28;
 library Engine {
     type Grid is uint16;
 
+    function equal(Grid self, Grid other) internal pure returns (bool) {
+        return Grid.unwrap(self) == Grid.unwrap(other);
+    }
+
     function toGrid(uint16 value) internal pure returns (Grid) {
         return Grid.wrap(value & 0x1ff);
     }
