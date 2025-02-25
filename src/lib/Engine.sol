5c62c6151b59d1486f92d1dfd1bfc5b69a15a177
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 01:24:22 -0500

    fix: add function Engine.get(Grid,uint256)

--- src/lib/Engine.sol
+++ src/lib/Engine.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.28;
 
+import {Panic} from "@openzeppelin/contracts/utils/Panic.sol";
+
 library Engine {
     type Grid is uint16;
 
@@ -13,6 +15,11 @@ library Engine {
     Grid private constant GRID_7 = Grid.wrap(0x0054); // 0b001010100
     Grid private constant GRID_8 = Grid.wrap(0x0111); // 0b100010001
 
+    modifier checkIndex(uint256 index) {
+        if (index >= 9) Panic.panic(Panic.ARRAY_OUT_OF_BOUNDS);
+        _;
+    }
+
     function isWinner(Grid self) internal pure returns (bool) {
         if (equal(intersection(self, GRID_1), GRID_1)) return true;
         if (equal(intersection(self, GRID_2), GRID_2)) return true;
@@ -23,6 +30,10 @@ library Engine {
         if (equal(intersection(self, GRID_7), GRID_7)) return true;
         if (equal(intersection(self, GRID_8), GRID_8)) return true;
         return false;
+    }
+
+    function get(Grid self, uint256 index) internal pure checkIndex(index) returns (bool) {
+        return ((Grid.unwrap(self) >> index) & 1) == 1;
     }
 
     function equal(Grid self, Grid other) internal pure returns (bool) {
