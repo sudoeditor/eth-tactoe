da68eaa5146fdf22473d8197fb90582d0bcc1d5b
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 14:05:22 -0500

    test: Engine.toGrid(uint16)

--- test/lib/Engine.t.sol
+++ test/lib/Engine.t.sol
@@ -5,4 +5,9 @@ import {Engine} from "../../src/lib/Engine.sol";
 
 import {Test} from "forge-std/Test.sol";
 
-contract EngineTest is Test {}
+contract EngineTest is Test {
+    function test_toGrid_ZeroesLowerSevenBits() public pure {
+        Engine.Grid grid = Engine.toGrid(0xffff); // 0b1111111111111111
+        assertEq(Engine.Grid.unwrap(grid), 0x1ff); // 0b0000000111111111
+    }
+}
