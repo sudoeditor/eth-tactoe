ac03a2c3e88164103adaa58f3c84d8f3bc5149e3
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 14:57:18 -0500

    refactor(test): define constants globally in lib/Engine.t.sol

--- test/lib/Engine.t.sol
+++ test/lib/Engine.t.sol
@@ -8,28 +8,35 @@ import {Test} from "forge-std/Test.sol";
 contract EngineTest is Test {
     using Engine for Engine.Grid;
 
-    function test_equal_ReturnsTrueWhenAllBitsMatchAndFalseOtherwise() public pure {
-        Engine.Grid grid_1 = Engine.toGrid(0x155); // 0b0000000101010101
-        Engine.Grid grid_2 = Engine.toGrid(0x155); // 0b0000000101010101
-        Engine.Grid grid_3 = Engine.toGrid(0x1ab); // 0b0000000110101011
-        assertTrue(grid_1.equal(grid_2));
-        assertFalse(grid_1.equal(grid_3));
+    Engine.Grid public gridA;
+    Engine.Grid public gridB;
+    Engine.Grid public gridC;
+
+    constructor() {
+        gridA = Engine.toGrid(0x155); // 0b0000000101010101
+        gridB = Engine.toGrid(0x0aa); // 0b0000000010101010
+        gridC = Engine.toGrid(0x1ab); // 0b0000000110101011
     }
 
-    function test_intersection_ResultContainsAllNonZeroBitsInBoth() public pure {
-        Engine.Grid grid_1 = Engine.toGrid(0x155); // 0b0000000101010101
-        Engine.Grid grid_2 = Engine.toGrid(0x1ab); // 0b0000000110101011
-        assertEq(Engine.Grid.unwrap(grid_1.intersection(grid_2)), 0x101); // 0b0000000100000001
+    function test_equal_ReturnsTrueWhenAllBitsMatchAndFalseOtherwise() public view {
+        assertTrue(gridA.equal(gridA));
+        assertFalse(gridA.equal(gridC));
     }
 
-    function test_union_ResultContainsAllNonZeroBitsFromBoth() public pure {
-        Engine.Grid grid_1 = Engine.toGrid(0x155); // 0b0000000101010101
-        Engine.Grid grid_2 = Engine.toGrid(0x0aa); // 0b0000000010101010
-        assertEq(Engine.Grid.unwrap(grid_1.union(grid_2)), 0x1ff); // 0b0000000111111111
+    function test_intersection_ResultContainsAllNonZeroBitsInBoth() public view {
+        assertEq(unwrap(gridA.intersection(gridC)), 0x101); // 0b0000000100000001
+    }
+
+    function test_union_ResultContainsAllNonZeroBitsFromBoth() public view {
+        assertEq(unwrap(gridA.union(gridB)), 0x1ff); // 0b0000000111111111
     }
 
     function test_toGrid_ZeroesLowerSevenBits() public pure {
         Engine.Grid grid = Engine.toGrid(0xffff); // 0b1111111111111111
-        assertEq(Engine.Grid.unwrap(grid), 0x1ff); // 0b0000000111111111
+        assertEq(unwrap(grid), 0x1ff); // 0b0000000111111111
+    }
+
+    function unwrap(Engine.Grid grid) internal pure returns (uint16) {
+        return Engine.Grid.unwrap(grid);
     }
 }
