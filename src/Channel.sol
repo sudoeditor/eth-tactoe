8492a4a2d71d87cfa75d67ce4edea88061ff8cda
sudoeditor <198113122+sudoeditor@users.noreply.github.com>
2025-02-25 22:09:55 -0500

    fix: add function Channel.close

--- src/Channel.sol
+++ src/Channel.sol
@@ -3,9 +3,21 @@ pragma solidity 0.8.28;
 
 import {EIP712Upgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
 
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 contract Channel is EIP712Upgradeable {
+    bytes32 private constant CLOSE_TYPEHASH = keccak256("Close(address winner)");
+
     address public alice;
     address public bob;
+    address public winner;
+
+    error AlreadyClosed();
+    error InvalidCaller();
+    error InvalidSigner();
+    error InvalidWinner();
+
+    event Closed(address indexed winner);
 
     constructor() {
         _disableInitializers();
@@ -19,5 +31,18 @@ contract Channel is EIP712Upgradeable {
         alice = _alice;
         // slither-disable-next-line missing-zero-check
         bob = _bob;
+    }
+
+    // slither-disable-next-line naming-convention
+    function close(address _winner, bytes calldata signature) external {
+        if (winner != address(0)) revert AlreadyClosed();
+        if (msg.sender != alice && msg.sender != bob) revert InvalidCaller();
+        if (_winner != alice && _winner != bob) revert InvalidWinner();
+
+        bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(CLOSE_TYPEHASH, _winner)));
+        address signer = ECDSA.recover(digest, signature);
+        if ((msg.sender == alice) ? signer != bob : signer != alice) revert InvalidSigner();
+
+        emit Closed(winner = _winner);
     }
 }
